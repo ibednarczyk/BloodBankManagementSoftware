8a42d08bcadc061959e26354f30f435c80376d1c
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2021-03-14 19:13:28 +0100

    Replacing unnecessary data base migrations

---
+++ BloodBankManagementSoftware/Migrations/20210314142540_Migration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloodBankManagementSoftware.Migrations
+{
+    public partial class FixedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donors",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RequestId = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    BloodGroup = table.Column<string>(type: "TEXT", nullable: true),
+                    RhFactorType = table.Column<char>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donors");
+        }
+    }
+}
