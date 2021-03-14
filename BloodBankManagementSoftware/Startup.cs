7590ac07e035f969e4c1a1bbd866d54b55bea95e
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2021-03-14 19:17:18 +0100

    Update EF Tools version + remove comments

--- BloodBankManagementSoftware/Startup.cs
+++ BloodBankManagementSoftware/Startup.cs
@@ -17,7 +17,6 @@ namespace BloodBankManagementSoftware
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -26,7 +25,6 @@ namespace BloodBankManagementSoftware
                     options.UseSqlite(Configuration.GetConnectionString("MvcDonorContext")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
