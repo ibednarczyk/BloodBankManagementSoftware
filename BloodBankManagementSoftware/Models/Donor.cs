43ba8caf23fa178713d2189057b8037c46f49eff
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2021-03-21 17:14:09 +0100

    Adding UI validation

--- BloodBankManagementSoftware/Models/Donor.cs
+++ BloodBankManagementSoftware/Models/Donor.cs
@@ -10,20 +10,31 @@ namespace BloodBankManagementSoftware.Models
 
         public long Id { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
 
+        [StringLength(60)]
+        [Required]
         public string Address { get; set; }
 
+        [Required]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
-
+        
+        [RegularExpression(@"[AB0]{1}")]
+        [StringLength(1)]
+        [Required]
         [Display(Name = "Blood Group")]
         public string BloodGroup { get; set; }
 
+        [RegularExpression(@"[+-]{1}")]
+        [Required]
         [Display(Name = "Rh Factor Type")]
         public char RhFactorType { get; set; }
 
-        [Display(Name = "E-Mail address")]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")]
+        [Display(Name = "E-mail address")]
         public string EMailAddress { get; set; }
     }
 }
